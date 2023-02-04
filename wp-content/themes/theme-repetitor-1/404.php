518415a73d28a08dc7dc1cb7300662a15a5cd578
pavel_blednov <pavel.blednov@andevart.com>
2023-02-04 18:52:17 +0200

    make 404 page

---
+++ wp-content/themes/theme-repetitor-1/404.php
@@ -0,0 +1,21 @@
+<?php
+	get_header();
+?>
+
+<section class="hide_line   section   section-404" style="padding-top:80px; padding-bottom:80px;">
+	<div class="section_inner width1170">
+		<div class="title">404</div>
+
+		<p>Страница не найдена</p>
+
+		<div class="section164">
+			<a href="<?php echo home_url(); ?>" class="btn1 surround" data-id="null" data-hcolor="#ed7a00"
+				style="background-color:#ed7a00;  border-radius: 2em;   box-shadow: none;  font-size: 18px;">
+				Вернуться на главную
+			</a>
+		</div>
+	</div>
+</section>
+
+<?php
+	get_footer();
